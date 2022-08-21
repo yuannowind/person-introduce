519cbbb6978210bf319ddccc97db6b0198399337
yanzhenpeng <1468148085@qq.com>
2022-08-21 16:06:01 +0800

    新增资源文件和渐变文字

--- app/src/main/java/com/love/introduce/MainActivity.kt
+++ app/src/main/java/com/love/introduce/MainActivity.kt
@@ -1,16 +1,26 @@
-package com.love.introduce;
+package com.love.introduce
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.love.introduce.R
+import com.love.introduce.databinding.ActivityMainBinding
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btStartanim.setOnClickListener {
+            binding.patv.startTextAnim()
+            binding.patv2.startTextAnim()
+            binding.patv3.startTextAnim()
+        }
+        binding.apply {
+            Glide.with(this@MainActivity)
+                .load(R.mipmap.empty_address)
+                .into(this.imgGif)
+        }
     }
 }
